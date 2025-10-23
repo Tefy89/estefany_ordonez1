acb8b76e6ca3a977638ab2e0bc022449418bea94
Tefy89 <eds.ordonez@yavirac.edu.ec>
2025-10-23 10:17:10 -0500

    cambiar puerto

--- index.js
+++ index.js
@@ -8,4 +8,15 @@ function resta(a, b) {
 
 console.log("Ejercicio demostrativo");
 
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('App corriendo correctamente');
+});
+
+server.listen(3080, () => {
+  console.log('Servidor escuchando en puerto 3000');
+});
+
+
 module.exports = { suma, resta };
