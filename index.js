d92aac7db038bb51b68f86050bfeac857cafe285
Tefy89 <eds.ordonez@yavirac.edu.ec>
2025-10-23 10:43:58 -0500

    cambiar puerto

--- index.js
+++ index.js
@@ -8,15 +8,30 @@ function resta(a, b) {
 
 console.log("Ejercicio demostrativo");
 
-// Servidor mínimo para mantener el contenedor vivo
-const http = require('http');
-const server = http.createServer((req, res) => {
-  res.end('App corriendo correctamente');
-});
+function suma(a, b) {
+  return a + b;
+}
 
-server.listen(3080, () => {
-  console.log('Servidor escuchando en puerto 3080');
-});
+function resta(a, b) {
+  return a - b;
+}
+
+console.log("Ejercicio demostrativo");
+
+const http = require('http');
+
+// Solo iniciar servidor si NO estamos corriendo tests
+if (!process.env.CI && process.env.JEST_WORKER_ID === undefined) {
+  const server = http.createServer((req, res) => {
+    res.end('App corriendo correctamente');
+  });
+
+  server.listen(3000, () => {
+    console.log('Servidor escuchando en puerto 3000');
+  });
+}
+
+module.exports = { suma, resta };
 
 
 module.exports = { suma, resta };
