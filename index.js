e3f34981a9519e9580ef60b671d2e10d7708de8c
Tefy89 <eds.ordonez@yavirac.edu.ec>
2025-10-23 10:24:53 -0500

    cambiar puerto

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function resta(a, b) {
 
 console.log("Ejercicio demostrativo");
 
+// Servidor mínimo para mantener el contenedor vivo
 const http = require('http');
-
 const server = http.createServer((req, res) => {
   res.end('App corriendo correctamente');
 });
 
-server.listen(3080, () => {
+server.listen(3000, () => {
   console.log('Servidor escuchando en puerto 3000');
 });
 
